602f8d0e3da36c8f1b010c19383380b6e91c401f
Capedantic <jonathonreading@gmail.com>
2021-01-29 06:50:08 -1000

    12

--- LocalLibrary/wsgi.py
+++ LocalLibrary/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.LocalLibrary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LocalLibrary.settings')
 
 application = get_wsgi_application()
