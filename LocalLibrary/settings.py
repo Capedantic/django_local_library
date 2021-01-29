1e6e896d038fcd27d6d6d44d2b6131f0e0153641
Capedantic <jonathonreading@gmail.com>
2021-01-29 06:11:35 -1000

    maybe

--- LocalLibrary/settings.py
+++ LocalLibrary/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['capedanticlibrary.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
